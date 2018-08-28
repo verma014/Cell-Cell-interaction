c064a2cd40adbba753a6eb1c810b5adddca5c1ee
vermack <verma014@umn.edu>
2018-08-28 14:28:42 -0500

    Update HypercubedConnectome.R

--- HypercubedConnectome.R
+++ HypercubedConnectome.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 #Open up the LR expression from the app/data/ folder for the connectome app. Change the LR.expr to LR.expr.original.txt to keep track of the file. 
+
 LRlist<-read.delim("LR.expr.original.txt")
 #Keep only the gene name and remove all the other columns if you dont have that data
 LRlist<-as.data.frame(LRlist[,1])
@@ -12,12 +13,6 @@ humanID$Human.gene.name<-toupper(humanID$Human.gene.name)
 LRlistEC_SC_SMF<-merge(LRlist,humanID[,c(2,5:7)],by="Human.gene.name",all.x = T)
 #
 UniqueLRMatrix<-LRlistEC_SC_SMF
-#COnvert to Log2
-#UniqueLRMatrix[,2:9]<-log2(UniqueLRMatrix[,2:9])
-#colnames(EC_Fantom5)[1]<-"Gene.Name"
-#U_EC_Fantom5<-distinct(EC_Fantom5,Gene.Name,.keep_all = TRUE)
-#LRlistEC_SC<-merge(U_LRlistSC,U_EC_Fantom5,by="Gene.Name",all.x = T)
-#UniqueLRMatrix<-LRlistEC_SC[,-5]
 colnames(UniqueLRMatrix)[1]<-"ApprovedSymbol"
 #Graph the density of individual expression values 
 graph<-UniqueLRMatrix[,c(2:4)]
@@ -32,3 +27,11 @@ ggplot(graph)+
   theme(plot.title = element_text(hjust = 0.5))
 #Copy the file into the app/data/ folder for the connectome app.
 write.table(UniqueLRMatrix,"LR.expr.txt", col.names = TRUE, sep = "\t", quote = FALSE,row.names = FALSE)
+
+#This will add the specificiity score for each samples to the average of each sample. This is a measure of how specific the marker is for a specific sample. This is not needed for hypercube, but may be useful for 
+LRlistSpecificity<-LRlistEC_SC_SMF
+LRlistSpecificity$median<-apply(LRlistEC_SC_SMF[,c(2:4)],1,median)
+LRlistSpecificity$ECSpecificity<-log10(LRlistSpecificity$ECaverage+1)-log10(LRlistSpecificity$median+1)
+LRlistSpecificity$SCSpecificity<-log10(LRlistSpecificity$SCaverage+1)-log10(LRlistSpecificity$median+1)
+LRlistSpecificity$SMFSpecificity<-log10(LRlistSpecificity$SMFaverage+1)-log10(LRlistSpecificity$median+1)
+write.csv(LRlistSpecificity,"LRlistSpecificity.csv")
